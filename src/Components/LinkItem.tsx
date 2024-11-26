9e745070e2c9f47bd902f03575c118c054ed3b83
Shahad Alsubhi <shahadalsubhi11@gmail.com>
2024-11-26 04:59:01 +0300

    update CSS styles

--- src/Components/LinkItem.tsx
+++ src/Components/LinkItem.tsx
@@ -24,7 +24,7 @@ const LinkItem = ({
     <div
       ref={setNodeRef}
       style={{ transition, transform: CSS.Transform.toString(transform) }}
-      className="p-5 rounded-lg bg-[#FAFAFA] mb-6 touch-none"
+      className="p-5 rounded-lg bg-[#FAFAFA] mb-6 "
     >
       <div className="overflow-hidden mb-3">
         <img
@@ -32,7 +32,7 @@ const LinkItem = ({
           {...listeners}
           src="/images/icon-drag-and-drop.svg"
           alt="icon-drag-and-drop"
-          className="inline mr-2"
+          className="inline mr-2 w-4"
         />
         <h4 className="inline font-semibold text-[#737373]">Link#{place}</h4>
         <button
--- src/Layout.tsx
+++ src/Layout.tsx
@@ -6,7 +6,7 @@ const Layout = () => {
   return (
     <>
       <Header />
-      <div className="xl:grid xl:grid-cols-[1fr_1.5fr] m-6 xl:gap-4 h-screen">
+      <div className="xl:grid xl:grid-cols-[1fr_1.5fr] m-6 xl:gap-4">
         <PhoneMockup />
         <Outlet />
       </div>
