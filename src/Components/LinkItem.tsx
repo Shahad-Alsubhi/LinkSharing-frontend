3b73f0922fd5575207461800d2736c6bd1c7d549
Shahad Alsubhi <shahadalsubhi11@gmail.com>
2024-11-28 01:41:45 +0300

    update CSS styles

--- src/Components/LinkItem.tsx
+++ src/Components/LinkItem.tsx
@@ -26,20 +26,21 @@ const LinkItem = ({
       style={{ transition, transform: CSS.Transform.toString(transform) }}
       className="p-5 rounded-lg bg-[#FAFAFA] mb-6 touch-none"
     >
-      <div className="overflow-hidden mb-3">
-        <span {...attributes} {...listeners}>
+      <div className="overflow-hidden mb-3 -m-5">
+        <span {...attributes} {...listeners} className="inline-block p-5 pb-2 ">
           <img
             src="/images/icon-drag-and-drop.svg"
             alt="icon-drag-and-drop"
             className="inline mr-2 w-4 select-none"
             draggable="false"
+            onTouchStart={(e) => e.preventDefault()}
           />
-          <h4 className="inline font-semibold text-[#737373] select-none">
+          <h4 className="inline-block font-semibold text-[#737373] select-none">
             Link#{place}
           </h4>
         </span>
         <button
-          className="float-right text-base text-[#737373] "
+          className="float-right text-base text-[#737373] p-5 pb-3 select-none "
           onClick={() => handleRemove(id)}
           aria-label={`Remove link at position ${place}`}
         >
