eacdfcb67f249f1efe88899e0cb22703a29e4251
Shahad Alsubhi <shahadalsubhi11@gmail.com>
2024-11-29 05:27:38 +0300

    Host Fonts Locally

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -48,7 +48,7 @@ const Header = () => {
               d="M11.154 14.65a.94.94 0 0 1 0 1.329l-.464.464a4.689 4.689 0 0 1-6.631-6.631l1.884-1.884a4.688 4.688 0 0 1 6.432-.194.941.941 0 0 1-1.25 1.407 2.812 2.812 0 0 0-3.857.115l-1.883 1.88a2.813 2.813 0 1 0 3.978 3.979l.464-.464a.937.937 0 0 1 1.327 0Zm5.788-11.093a4.695 4.695 0 0 0-6.632 0l-.464.464a.94.94 0 0 0 1.328 1.328l.464-.464a2.813 2.813 0 1 1 3.979 3.978l-1.884 1.885a2.812 2.812 0 0 1-3.858.112.94.94 0 1 0-1.25 1.406 4.688 4.688 0 0 0 6.43-.19l1.884-1.884a4.695 4.695 0 0 0 .003-6.633v-.002Z"
             />
           </svg>
-          <span className={`max-md:hidden text-base font-semibold `}>
+          <span className={`max-md:hidden font-semibold `}>
             Links
           </span>
         </button>
@@ -76,13 +76,13 @@ const Header = () => {
               d="M10.5 1.563A8.437 8.437 0 1 0 18.938 10 8.447 8.447 0 0 0 10.5 1.562ZM6.716 15.357a4.688 4.688 0 0 1 7.568 0 6.54 6.54 0 0 1-7.568 0Zm1.596-5.982a2.188 2.188 0 1 1 4.376 0 2.188 2.188 0 0 1-4.376 0Zm7.344 4.683a6.523 6.523 0 0 0-2.265-1.83 4.062 4.062 0 1 0-5.782 0 6.522 6.522 0 0 0-2.265 1.83 6.562 6.562 0 1 1 10.304 0h.008Z"
             />
           </svg>
-          <span className={`max-md:hidden text-base font-medium`}>
+          <span className={`max-md:hidden font-medium`}>
             Profile Details
           </span>
         </button>
       </nav>
 
-      <Button onClick={() => navigate("/preview")} variant="secondary">
+      <Button  onClick={() => navigate("/preview")} variant="secondary">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           width="20"
